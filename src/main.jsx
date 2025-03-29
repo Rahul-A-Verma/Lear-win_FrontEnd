fc769beb52edf562cbe33b5b92eb04343e82d2b6
Rahul-A-Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2025-03-30 00:01:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 // export const server = "http://localhost:3000";
-export const server = "https://learn-win.onrender.com/";
+export const server = "https://learn-win.onrender.com";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
