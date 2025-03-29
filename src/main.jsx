cf757d3ff9c57d2b95203c288679495ea31e755e
Rahul-A-Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2025-03-30 00:00:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
-export const server = "http://localhost:3000";
+// export const server = "http://localhost:3000";
+export const server = "https://learn-win.onrender.com/";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
